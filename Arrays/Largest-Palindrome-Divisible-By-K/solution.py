e76b207e50e24c1fff50e7ea08bcef60fc35ea87
Krupen Patel <pen878695@gmail.com>
2026-06-05 12:51:28 +0530

    Clean up solution implementation

--- Arrays/Largest-Palindrome-Divisible-By-K/solution.py
+++ Arrays/Largest-Palindrome-Divisible-By-K/solution.py
@@ -1,1 +1,21 @@
+class Solution(object):
 
+    def isPalindrome(self, x):
+        temp = x
+        rev = 0
+
+        while temp != 0:
+            d = temp % 10
+            rev = rev * 10 + d
+            temp = temp // 10
+
+        return rev == x
+
+    def check(self, n, k):
+
+        for i in range(10**n - 1, 10**(n - 1) - 1, -1):
+
+            if self.isPalindrome(i):
+
+                if i % k == 0:
+                    return i
